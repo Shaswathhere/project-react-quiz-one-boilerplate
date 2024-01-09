40dd79c807d4d9a2f0f86898f4413ba1552cfa51
Shaswath <shaswath.g@kalvium.community>
2024-01-09 10:10:14 +0530

    done

---
+++ quiz/src/App.jsx
@@ -0,0 +1,18 @@
+import Home from './Components/Home'
+import Quiz from './Components/Quiz'
+import Result from './Components/Result'
+import './App.css'
+
+function App() {
+  return(
+    <div className='main'>
+          <Home/>
+          <Quiz/>
+          <Result/>
+    </div>
+
+  )
+  
+}
+
+export default App
---
+++ quiz/src/Components/Quiz.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import './Quiz.css'
+
+class Quiz extends Component{
+    render(){
+        return(
+            <div className="quiz">
+                <h1>Question</h1>
+                <p id="questionNumber">1 of 15</p>
+                <h4 id="question">Which is the only mammal than can jump ?</h4>
+                <div id="Optionbuttons">
+                    <button>Dog</button>
+                    <button>Elephant</button>
+                    <button>Goat</button>
+                    <button>Lion</button>
+                </div>
+                <div id ='navigation'>
+                    <button id='prevBtn'>Previous</button>
+                    <button id='nextBtn'>Next</button>
+                    <button id='quitBtn'>Quit</button>
+                </div>
+            </div>
+        )
+    }
+}
+export default Quiz
+
+
